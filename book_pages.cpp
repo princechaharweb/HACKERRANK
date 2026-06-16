a5fc970ac4c267efa5a2256142e5249a27299325
Prince Chahar <princechahar456@gmail.com>
2026-06-16 16:54:01 +0530

    hackerrank problem 12

---
+++ book_pages.cpp
@@ -0,0 +1,29 @@
+// BOOK PAGES ###########################################################################
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int i,p,n;
+//     cin>>n;
+//     cin>>p;
+//     int front;
+//     int back;
+//     if(p-1<n-p){
+//         cout<<p/2; 
+//     }
+//     if(p-1==n-p){
+//         if(p/2<(n-p)/2){
+//             cout<<p/2;
+//         }
+//         else{
+//             cout<<(n-p)/2;
+//             }
+//     }
+//     if(p-1>n-p){
+//     if(n%2==0){
+//        cout<<(n-p+1)/2;
+//     }
+//     else{
+//         cout<<(n-p)/2;
+//     }
+//     }
+// }

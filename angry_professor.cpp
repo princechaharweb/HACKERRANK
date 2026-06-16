1407285a206ce59aa54c354614b2a7926624d55a
Prince Chahar <princechahar456@gmail.com>
2026-06-16 16:54:52 +0530

    hackerrank problem 18

---
+++ angry_professor.cpp
@@ -0,0 +1,32 @@
+// ANGRY PROFESSOR #######################################################################
+// #include <iostream>
+// using namespace std;
+// int main(){
+//      int t,i,j,p;
+//      cin>>t;
+//      int arr[1000];
+//      int n[1000];
+//      int k[1000];
+    
+//      for(i=0;i<t;i++){
+//          int flag=0;
+//         cin>>n[i]>>k[i];
+//         for(j=0;j<n[i];j++){
+//             cin>>arr[j];
+//         }
+//         for(p=0;p<n[i];p++){
+//             if(arr[p]<=0){
+//                 flag++;
+//             }
+//         }
+//         if(flag>=k[i]){
+//             cout<<"NO"<<endl;
+//         }
+//         else{
+//             cout<<"YES"<<endl;
+//         }
+        
+//      }
+
+  
+//   }

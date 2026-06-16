efbfe3644077dbb6d6a917ed6006b6dc7b63b300
Prince Chahar <princechahar456@gmail.com>
2026-06-16 16:55:09 +0530

    hackerrank problem 19

---
+++ chocolate.cpp
@@ -0,0 +1,25 @@
+// Chocolate/SUBARRAY  *******************************************************************
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int i,j,n,d,m;
+//     cin>>n;
+//     int flag =0;
+//     int arr[n];
+//     for(i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     cin>>d>>m;
+//     int sum = 0;
+//     for(i=0;i<n-m;i++){
+//           sum=0;
+//         for(j=i;j<i+m;j++){
+//              sum = sum + arr[j];     
+//         }
+//         if(sum==d){
+//             flag++;
+//         }  
+//     }
+//     cout<<flag;
+    
+// }

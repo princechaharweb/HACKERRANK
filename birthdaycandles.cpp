44c6673da358c13ad98b90eac3f3447a2a1b199a
Prince Chahar <princechahar456@gmail.com>
2026-06-16 16:45:17 +0530

    hackerrank problem 6

---
+++ birthdaycandles.cpp
@@ -0,0 +1,24 @@
+// BIRTHDAY CANDLES    #####################################################################
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n,i;
+//     int flag = 0;
+//     cin>>n;
+//     int arr[n];
+//     for(i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     int largest = arr[0];
+//     for(i=1;i<n;i++){
+//         if(arr[i]>largest){
+//             largest=arr[i];
+//         }
+//     }
+//     for(i=0;i<n;i++){
+//         if(arr[i]==largest){
+//             flag++;
+//         }
+//     }
+//     cout<<flag;  
+// }

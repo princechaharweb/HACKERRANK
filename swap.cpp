0eaaa061b0d0794e2625f1492b04ac5e46b677d6
Prince Chahar <princechahar456@gmail.com>
2026-06-16 16:02:06 +0530

    Hackkerank problem 1

---
+++ swap.cpp
@@ -0,0 +1,17 @@
+// SWAP ##################################################################################
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     string a;
+//     string b;
+//     cin >> a ;
+//     cin >> b ;
+//     cout << a.length() << " " << b.length() << endl ;
+//     string c = a + b ;
+//     cout << c << endl ;
+//     char temp = a[0];
+//     a[0] = b[0];
+//     b[0] =  temp ;
+//     cout<<a << " " << b;
+//     return 0;    
+// //     }

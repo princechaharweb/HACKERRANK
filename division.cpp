870f516c09526b5de22c5fd85ee1c3ac45c5878d
Prince Chahar <princechahar456@gmail.com>
2026-06-16 16:53:08 +0530

    hackerrank problem 7

---
+++ division.cpp
@@ -0,0 +1,21 @@
+// DIVISION IN ARRAY ####################################################################
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int i,j,n,k;
+//     cin>>n;
+//     cin>>k;
+//     int flag=0;
+//     int arr[n];
+//     for(i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     for(i=0;i<n;i++){
+//         for(j=1+i;j<n;j++){
+//             if((arr[i]+arr[j])%k ==0){
+//                 flag++;
+//             }
+//         }
+//     }
+//     cout<<flag;
+// }

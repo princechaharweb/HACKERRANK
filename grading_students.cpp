e2ce1e9644e9da600e0e33f75df76f36a4307667
Prince Chahar <princechahar456@gmail.com>
2026-06-16 16:54:37 +0530

    hackerrank problem 16

---
+++ grading_students.cpp
@@ -0,0 +1,27 @@
+// GRADING STUDENTS   ###################################################################
+// #include <iostream>
+// #include <cmath>
+// using namespace std;
+// int main(){
+//     int n,i;
+//     cin>>n;
+//     int arr[n];
+//     for(i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     int grade = (arr[i]/5 + 1)*5;
+//     for(i=0;i<n;i++){
+//         if(arr[i]>=38){
+//         if(((arr[i]/5 + 1)*5 - arr[i])<3){
+//             cout<<(arr[i]/5 + 1)*5<<endl;  
+//         }
+//         else{
+//             cout<<arr[i]<<endl;
+//         }
+//      }
+//      else{
+//         cout << arr[i]<<endl;
+//      }
+       
+//     }
+// }

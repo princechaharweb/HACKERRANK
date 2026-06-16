b7aa397050506ee39985a0153d9654197b06e95e
Prince Chahar <princechahar456@gmail.com>
2026-06-16 16:55:02 +0530

    hackerrank problem 18

---
+++ cat_mouse.cpp
@@ -0,0 +1,24 @@
+// CAT AND MOUSE #########################################################################
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n,i,j,q;
+//     cin>>n;
+//     int arr [n][3];
+//     for(i=0;i<n;i++){
+//         for(j=0;j<3;j++){
+//             cin>>arr[i][j];
+//         }
+//     }
+//     for(i=0;i<n;i++){
+//         if(abs(arr[i][0]-arr[i][2])>abs(arr[i][1]-arr[i][2])){
+//             cout<<"Cat B"<<endl;
+//         }
+//         else if(abs(arr[i][0]-arr[i][2])==abs(arr[i][1]-arr[i][2])){
+//             cout<<"Mouse C"<<endl;
+//         }
+//         else{
+//             cout<<"Cat A"<<endl;
+//         }
+//     }
+//     }

7554af35bbc627f1e883bddf75050bfe71c99fba
Prince Chahar <princechahar456@gmail.com>
2026-06-16 16:54:29 +0530

    hackerrank problem 15

---
+++ electronics_shop.cpp
@@ -0,0 +1,28 @@
+// ELECTRONICS SHOP  ####################################################################
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int i,n,m,j;
+//     long long b;
+//     cin>>b>>n>>m;
+//     long long key[n];
+//     long long dri[m];
+//     for(i=0;i<n;i++){
+//         cin>>key[i];
+//     }
+//     for(i=0;i<m;i++){
+//         cin>>dri[i];
+//     }
+//     long long sum;
+//     long long max = -1;
+//     for(i=0;i<n;i++){
+//         for(j=0;j<m;j++){
+//              sum = key[i] + dri[j];
+//         if(sum<=b && sum>=max){
+//             max=sum;
+//         }
+//         }
+//     }
+    
+//         cout<<max;   
+// }

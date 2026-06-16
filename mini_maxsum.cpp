01b7d1f6f10d73550db2c4967486c3aa78cf4ba2
Prince Chahar <princechahar456@gmail.com>
2026-06-16 16:53:35 +0530

    hackerrank problem 9

---
+++ mini_maxsum.cpp
@@ -0,0 +1,28 @@
+// MINI MAX SUM    #####################################################################
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n,i;
+//     int arr[5];
+//     for(i=0;i<5;i++){
+//         cin>>arr[i];
+//     }
+//     int maximum = arr[0];
+//     for(i=1;i<5;i++){
+//         if(arr[i]>maximum){
+//             maximum = arr[i];
+//         }
+//     }
+//     int minimum = arr[0];
+//     for(i=1;i<5;i++){
+//         if(arr[i]<minimum){
+//             minimum = arr[i];
+//         }
+//     }
+//     int sum = 0;
+//     for(i=0;i<5;i++){
+//         sum += arr[i];
+//     }
+//     cout<<sum-maximum<<" "<<sum-minimum;
+//     return 0;
+// }

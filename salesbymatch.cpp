3ea411b44fe7235f753998ad586d7bcc360e4f80
Prince Chahar <princechahar456@gmail.com>
2026-06-16 16:53:52 +0530

    hackerrank problem 11

---
+++ salesbymatch.cpp
@@ -0,0 +1,18 @@
+// SALES BY MATCH #######################################################################
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int i,n,j;
+//     cin>>n;
+//     int arr[n];
+//     int flag[1000]={0};
+//     for(i=0;i<n;i++){
+//         cin>>arr[i];
+//         flag[arr[i]]++;
+//     }
+//     int sum=0;
+//     for(i=0;i<1000;i++){
+//         sum = sum + flag[i]/2;
+//     }
+//     cout<<sum;
+// }

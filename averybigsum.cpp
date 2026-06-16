7696e4884317eec1cdbed441f95ca85050b25ade
Prince Chahar <princechahar456@gmail.com>
2026-06-16 16:04:44 +0530

    Hackkerank problem 2

---
+++ averybigsum.cpp
@@ -0,0 +1,17 @@
+// A VERY BIG SUM  ########################################################################
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n,i;
+//     cin>>n;
+//     long long arr[n];
+//     for(i=0;i<n;i++){
+//     cin>> arr[i];
+//      }
+//     long long  sum = 0;
+//     for(i=0;i<n;i++){
+//       sum = sum + arr[i];
+//     }
+//     cout<< sum ;
+//     return 0;
+// }

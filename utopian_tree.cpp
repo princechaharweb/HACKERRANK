2c81ddb89883e2b1075587060dc825932012c4fc
Prince Chahar <princechahar456@gmail.com>
2026-06-16 16:55:50 +0530

    hackerrank problem 19

---
+++ utopian_tree.cpp
@@ -0,0 +1,25 @@
+// UTOPIAN TREE #########################################################################
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int t,n,i,j;
+//     cin>>t;
+//     int arr[t];
+//     for(i=0;i<t;i++){
+//         cin>>arr[i];
+//     }
+//     int h = 0;
+    
+//     for(i=0;i<t;i++){
+//         int answer=0;
+//     for(j=0;j<=arr[i];j++){
+//         if(j%2==0){
+//             answer=answer+1;
+//         }
+//         else{
+//             answer=answer*2;
+//         }
+//     }
+//     cout<<answer<<endl;
+//     }
+// }

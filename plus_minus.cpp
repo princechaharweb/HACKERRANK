7bca4ac8eb751d8675e078a84a132383e472c6de
Prince Chahar <princechahar456@gmail.com>
2026-06-16 16:05:01 +0530

    Hackerrank problem 3

---
+++ plus_minus.cpp
@@ -0,0 +1,33 @@
+// PLUS MINUS  #############################################################################
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n,i;
+//     float flag1 =0;
+//     float flag2 =0;
+//     float flag3 =0;
+//     cin>>n;
+//     int arr[n];
+//     for(i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     for(i=0;i<n;i++){
+//         if(arr[i]>0){
+//             flag1++;     
+//         }
+//         else if(arr[i]<0){
+//             flag2++;     
+//         }
+//         else{
+//             flag3++;  
+//           }
+//     }
+//     float sump = flag1/n;
+//     float sumn = flag2/n;
+//     float sumz = flag3/n;
+    
+//     cout << sump << endl;
+//     cout << sumn << endl;
+//     cout << sumz << endl;
+//      return 0;
+// }

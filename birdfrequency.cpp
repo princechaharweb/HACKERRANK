7a13e17a28b3efdefbfc696529e6c99dad30ac3f
Prince Chahar <princechahar456@gmail.com>
2026-06-16 16:54:10 +0530

    hackerrank problem 13

---
+++ birdfrequency.cpp
@@ -0,0 +1,22 @@
+// BIRD FREQUENCY ************************************************************************
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int i,j,n;
+//     cin>>n;
+//     int arr[n];
+//     int flag[6]={0};
+//     for(i=0;i<n;i++){
+//         cin>>arr[i];
+//         flag[arr[i]]++;
+//     }
+//     int max=flag[1];
+//     int answer=1;
+//     for(i=2;i<=5;i++){
+//         if(flag[i]>max){
+//             max=flag[i];
+//             answer=i;
+//         }
+//     }
+//     cout<<answer;
+//     }

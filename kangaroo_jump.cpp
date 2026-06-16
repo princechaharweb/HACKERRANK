c9816f201e2a354fb9ec22548a68e92d88113199
Prince Chahar <princechahar456@gmail.com>
2026-06-16 16:54:45 +0530

    hackerrank problem 17

---
+++ kangaroo_jump.cpp
@@ -0,0 +1,20 @@
+// KANGAROO JUMP  #########################################################################
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x1,v1,x2,v2,i;
+//     int flag = 0;
+//     cin>>x1>>v1>>x2>>v2;
+//     for(i=0;i<10000;i++){
+//         if((x1+i*v1) - (x2+i*v2) == 0){
+//             flag++;
+//         }
+//     }
+//     if(flag>=1){
+//         cout<<"YES";
+//     }
+//     else{
+//         cout<<"NO";
+//     }
+//     return 0;
+// }

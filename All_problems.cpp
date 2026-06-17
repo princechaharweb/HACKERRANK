833955e1ca653cb46d7b4b100084424a6288b41e
Prince Chahar <princechahar456@gmail.com>
2026-06-17 22:36:40 +0530

    new problem solved

--- All_problems.cpp
+++ All_problems.cpp
@@ -664,4 +664,32 @@
 //     cout<<sum;
 // }
 
+// PRISIONER PROBLEM   #################################################################
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int i,j,n,t,k;
+//     cin>>t;
+//     int arr[3];
+//     for(i=0;i<t;i++){
+//         for(j=0;j<3;j++){
+//             cin>>arr[j];
+//         }
+//     int candy = arr[1];
+//     int prisioner = arr[0];
+//     int starting = arr[2]-1;
+    
+//    starting = (starting + candy)% prisioner;
+//    if(starting==0){
+//     cout<<prisioner<<endl;
+//    }
+//    else{
+//     cout<< starting<<endl;
+//    }
+//     }
+    
+// }
+
+
+
 

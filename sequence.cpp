feb1699f043bc76841afa13c8f6ecc80efffaa4c
Prince Chahar <princechahar456@gmail.com>
2026-06-17 23:35:04 +0530

    hackerrank problem 23

---
+++ sequence.cpp
@@ -0,0 +1,26 @@
+// SEQUENCE PROBLEM    ###################################################################
+// #include <iostream>
+// using namespace std;
+// int main (){
+//     int i,j,n,k;
+//     cin>>n;
+//     int arr[n];
+//     int answer;
+//     for(i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     for(i=1;i<=n;i++){
+//         int count=0;
+//     for(j=0;j<n;j++){
+//         if(i==arr[j]){
+//             count=j+1;
+//         }
+//         for(k=0;k<n;k++){
+//             if(count==arr[k]){
+//                 answer=k+1;
+//             }
+//         }
+//     }
+//     cout<<answer<<endl;  
+//     }
+// }

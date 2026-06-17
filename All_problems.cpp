26ec8ec3ac4b94534f6d9ecb5259a63e913a6243
Prince Chahar <princechahar456@gmail.com>
2026-06-17 21:25:48 +0530

    new problem solved

--- All_problems.cpp
+++ All_problems.cpp
@@ -647,3 +647,21 @@
 // }
 
 
+//VIRAL ADVERTISING   ###################################################################
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n,i,j;
+//     cin>>n;
+//     int sum = 0;
+//     int shared=5;
+//     int liked;
+//     for(i=1;i<=n;i++){
+//         liked = shared/2;
+//         shared = liked*3;
+//     sum = sum + liked;
+//     }
+//     cout<<sum;
+// }
+
+

70f559edb391ec93528f5e2da0275f55bba4bec6
Prince Chahar <princechahar456@gmail.com>
2026-06-18 22:57:25 +0530

    new problem solved

--- All_problems.cpp
+++ All_problems.cpp
@@ -756,3 +756,35 @@
 // }
 
 
+
+
+// //FIND DIGIT  #########################################################################
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int i,j,n;
+//     cin>>n;
+//     int flag=0;
+//     int digit;
+//     int number;
+//     int arr[n];
+//     for(i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     for(i=0;i<n;i++){
+//         flag=0;
+//         number=arr[i];
+//         while(number!=0){
+//             digit=number%10;
+//         if(digit!=0 && arr[i]%digit==0){
+//             flag++;
+//         }
+//         number = number/10;
+//         }
+//         cout<<flag<<endl;
+//     }
+    
+// }
+
+
+

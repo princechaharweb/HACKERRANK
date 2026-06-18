d2370a39a514dbac6f4e5614fb559fd59763bc13
Prince Chahar <princechahar456@gmail.com>
2026-06-18 21:48:17 +0530

    hackerrank problem 24

---
+++ cloud_jump.cpp
@@ -0,0 +1,33 @@
+//CLOUD JUMP   ##########################################################################
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int i,j,n,k,p;
+//     cin>>n>>k;
+//     int arr[n];
+//     for(p=0;p<n;p++){
+//         cin>>arr[p];
+//     }
+//     i=0;
+//     int energy = 100;
+//     int cloud = (i+k)%n;
+//     while(cloud!=0){
+//         if(arr[cloud]==1){
+//             energy--;
+//             energy--;
+//             energy--;
+//         }
+//         else{
+//             energy--;
+//         }
+//         cloud = (cloud+k)%n;
+//     }
+//     if(arr[0]==1){
+//         cout<<energy-3;
+//     }
+//     else{
+//         cout<<energy-1;
+//     }
+    
+    
+// }

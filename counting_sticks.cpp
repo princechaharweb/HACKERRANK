653dae4a47b645dace73ceeee4bbafa4288810f8
Prince Chahar <princechahar456@gmail.com>
2026-06-19 21:58:12 +0530

    hackerrank problem 26

---
+++ counting_sticks.cpp
@@ -0,0 +1,36 @@
+//COUNTING STICKS   #####################################################################
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int i,j,n,k,m;
+//     cin>>n;
+//     int arr[n];
+//     for(i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     int count ;
+//     for(i=0;i<n;i++){
+//         count = 0;
+//         for(j=0;j<n;j++){
+//         if(arr[j]!=0){
+//             count++;
+//         }
+//         }
+//         if(count!=0){
+//             cout<<count<<endl;
+//         }
+//         int min = 100000;
+//         for(k=0;k<n;k++){
+//             if(arr[k]<min && arr[k]>0){
+//                 min=arr[k];
+//             }
+//         }
+//         for(m=0;m<n;m++){
+//             if(arr[m]>0){
+//             arr[m]=arr[m]-min;
+//             }
+            
+//         } 
+        
+//     }
+// }
